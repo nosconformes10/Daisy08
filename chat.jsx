60747d8bd408b62e51e7e786dbe19c8421ae476d
nosconformes10 <nosconformes10@gmail.com>
2025-07-28 01:02:38 +0100

    chat.jsx

---
+++ chat.jsx
@@ -0,0 +1,8 @@
+export default function Chat() {
+  return (
+    <div className="p-4 text-white bg-gradient-to-r from-gray-700 via-gray-900 to-black min-h-screen">
+      <h2 className="text-2xl font-bold text-yellow-400">Bem-vindo ao Daisy 08</h2>
+      <p className="mt-4">Área de mensagens em construção...</p>
+    </div>
+  );
+}
